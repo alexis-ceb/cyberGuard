ea3127943c365606c66d2e3640cab925859407a8
Alexis C. <alexisceballosc@gmail.com>
2024-04-02 14:25:05 -0600

    beta commits

--- electron/preload.js
+++ electron/preload.js
@@ -1,9 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electron', {
-  on: (channel, callback) =>
-    ipcRenderer.on(channel, callback),
-  send: (channel, args) =>
-    ipcRenderer.send(channel, args)
-
+  on: (channel, callback) => ipcRenderer.on(channel, callback),
+  send: (channel, args) => ipcRenderer.send(channel, args)
 })
