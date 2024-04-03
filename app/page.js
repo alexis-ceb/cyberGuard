5fe8d54f446453fb6bdbaf4f4437e23b34296a21
KiraBelak <kira@kontigo.lat>
2024-04-02 18:16:49 -0600

    Refactor page.js component

--- app/page.js
+++ app/page.js
@@ -4,12 +4,20 @@ import Header from '@/components/common/header'
 export default function Home() {
   return (
     <>
-    <Header />
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-4xl font-bold'>Welcome to CyberGuard AI</h1>
-      <Image src='/images/cyberguard-ai-logo.png' width={200} height={200} alt='CyberGuard AI Logo' />
-      <p className='text-lg'>CyberGuard AI is a cybersecurity company that provides a range of services to help protect your business from cyber threats.</p>
-    </main>
+      <Header />
+      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <h1 className='text-4xl font-bold'>Welcome to CyberGuard AI</h1>
+        <Image
+          src='/images/cyberguard-ai-logo.png'
+          width={200}
+          height={200}
+          alt='CyberGuard AI Logo'
+        />
+        <p className='text-lg'>
+          CyberGuard AI is a cybersecurity company that provides a range of
+          services to help protect your business from cyber threats.
+        </p>
+      </main>
     </>
   )
 }
