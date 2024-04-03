cc199608f5365a3596f18e59add8c70bc0688738
Alexis C. <alexisceballosc@gmail.com>
2024-04-03 13:20:00 -0600

    vite electron

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electron', {
-  on: (channel, callback) => ipcRenderer.on(channel, callback),
-  send: (channel, args) => ipcRenderer.send(channel, args)
-})
