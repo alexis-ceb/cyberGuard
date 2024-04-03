4a51099e24f3fe30556aff1410d39540e33629b2
AlejandroChavezA <129823059+AlejandroChavezA@users.noreply.github.com>
2024-04-03 13:39:43 -0600

    test

--- src/main/index.js
+++ src/main/index.js
@@ -2,7 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-
+//source:
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 900,
