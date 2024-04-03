bffcc8c9c4e848ce58e4e8cd20c2ef5e5dd93bf2
Alexis C. <alexisceballosc@gmail.com>
2024-04-03 14:41:23 -0600

    beta

--- src/renderer/src/pages/app.jsx
+++ src/renderer/src/pages/app.jsx
@@ -1,10 +1,13 @@
 import { Tabs, Tab } from '@nextui-org/react'
 import Page_1 from './page_1'
 import Page_2 from './page_2'
+import photo from '../../../../resources/logo.png'
+import { Image } from '@nextui-org/react'
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-white">
+      <Image src={photo} width={100} />
       <Tabs aria-label="Options">
         <Tab key="dashboard" title="Dashboard">
           <Page_1 />
