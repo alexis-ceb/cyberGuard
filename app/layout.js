7e75e5c85a6009e97dd168d559fa82c6aeb7e6da
KiraBelak <kira@kontigo.lat>
2024-04-02 18:16:45 -0600

    Update body background color in RootLayout

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className={`bg-[#F1F2F3] ${inter.className}`}>
         <Providers>{children}</Providers>
       </body>
     </html>
