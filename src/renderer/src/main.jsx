40f01ee0874d26670c326f5e170d98980ab8b5da
Alexis C. <alexisceballosc@gmail.com>
2024-04-03 14:04:43 -0600

    test

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -2,12 +2,17 @@ import './styles/globals.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { NextUIProvider, Button } from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react'
+import Tabs from './components/tabs'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <Button>ola</Button>
+      <div className="h-screen w-full bg-stone-200 px-4 py-6">
+        <div className="h-full w-full bg-white p-2 rounded-lg">
+          <Tabs />
+        </div>
+      </div>
     </NextUIProvider>
   </React.StrictMode>
 )
